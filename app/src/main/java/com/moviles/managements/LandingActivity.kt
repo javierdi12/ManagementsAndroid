25ee6c0eef8364b9a5a3bb6fef1aa0b7ed0cc9ab
javierdi12 <darlendiaz1212@gmail.com>
2025-05-17 04:33:05 -0600

    Implementacion final, todo completo

--- app/src/main/java/com/moviles/managements/LandingActivity.kt
+++ app/src/main/java/com/moviles/managements/LandingActivity.kt
@@ -1,7 +1,12 @@
 package com.moviles.managements
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -22,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.moviles.managements.ui.theme.ManagementsTheme
 import androidx.compose.ui.platform.LocalContext
+import com.google.firebase.messaging.FirebaseMessaging
 
 
 import com.moviles.managements.ui.theme.screens.StudentDetailActivity
@@ -29,6 +35,8 @@ import com.moviles.managements.ui.theme.screens.StudentDetailActivity
 class LandingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        createNotificationChannel(this)
+        subscribeToTopic()
         enableEdgeToEdge()
         setContent {
             ManagementsTheme {
@@ -37,6 +45,24 @@ class LandingActivity : ComponentActivity() {
         }
     }
 }
+
+fun createNotificationChannel(context: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channelId = "event_reminder_channel"
+        val channelName = "Event Reminders"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+        val channel = NotificationChannel(channelId, channelName, importance).apply {
+            description = "Notifies users about upcoming events"
+        }
+
+        val notificationManager =
+            context.getSystemService(NotificationManager::class.java)
+        notificationManager?.createNotificationChannel(channel)
+    }
+}
+
+
 
 @Composable
 fun LandingScreen() {
@@ -81,3 +107,15 @@ fun LandingScreen() {
         }
     }
 }
+
+fun subscribeToTopic() {
+    FirebaseMessaging.getInstance().subscribeToTopic("event_notifications")
+        .addOnCompleteListener { task ->
+            var msg = "Subscription successful"
+            if (!task.isSuccessful) {
+                msg = "Subscription failed"
+            }
+            Log.d("FCM", msg)
+        }
+}
+
